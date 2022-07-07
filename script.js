830a30cc883cc77360435fdfeba180c204a17ac9
benjaaperez8 <86675477+benjaaperez8@users.noreply.github.com>
2022-07-07 17:48:04 -0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert(2);
+function limpiar(){
+    document.getElementsById('mi formulario').reset();
+}
+
+function sumar(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x+y;
+    
+    
+}
+
+function restar(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x-y;
+}
+
+function multiplicar(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x*y;
+}
+function dividir(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x/y;
+}
